c6343981f9adde5bc38da8589000bacdf0ce4729
Mudit Singh <muditsingh6624@gmail.com>
2025-06-08 23:21:10 +0530

    data modelling

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -17,7 +19,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -48,5 +50,46 @@ const userSchema = new Schema(
     timestamps: true, //for created at, updated at
   }
 );
+
+// .pre -> any method we can call just before sending data into database
+// it takes first an "operation" and second a function
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  //
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+//comparing password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRT,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRT,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
